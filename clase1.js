3c8c6334f97540a65c80eadf6b172a15e138fb7d
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-09 06:22:33 -0600

    v1: Esta es mi primer clase con js

---
+++ clase1.js
@@ -0,0 +1,19 @@
+var sacha = {
+    apellido: 'Lifszyc',
+    nombre: 'Sacga',
+    edad: 23
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Menendez',
+    edad: 27
+}
+
+function imprimirNombreEnMayusculas(nombre){
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+
+imprimirNombreEnMayusculas(nombreSacha)
+imprimirNombreEnMayusculas(nombreDario)
