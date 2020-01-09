0a31aec30fb7de39b0042a72393cbb4bf6dfdada
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-09 08:40:19 -0600

    v2: intregue los arrow functions

--- clase1.js
+++ clase1.js
@@ -10,10 +10,52 @@ var dario = {
     edad: 27
 }
 
-function imprimirNombreEnMayusculas(nombre){
-    nombre = nombre.toUpperCase()
-    console.log(nombre)
+var oscar = {
+    nombre: 'oscar',
+    apellido: 'de La Rosa',
+    edad: 25
+
 }
 
-imprimirNombreEnMayusculas(nombreSacha)
-imprimirNombreEnMayusculas(nombreDario)
+function imprimirNombreEnMayusculas(persona){
+    var {nombre} = persona
+    console.log(nombre.toUpperCase())
+}
+
+function imprimirNombreyEdad(persona){
+    var {nombre} = persona
+    var {edad} = persona 
+    console.log('Hola soy ' + nombre + ' y tengo ' + edad + ' años')
+}
+
+function cumpleaños(persona){
+    persona.edad += 1
+}
+
+function nuevapersona(persona){
+    return{
+        ...persona,
+        nombre: 'Oscar Eduardo',
+        edad: persona.edad + 2,
+        profesion: 'programador'
+
+
+    }
+
+}
+
+function duplicate (persona1, persona2){
+ persona2.edad = 2 * persona1.edad
+}
+
+imprimirNombreEnMayusculas(sacha)
+imprimirNombreEnMayusculas(dario)
+imprimirNombreEnMayusculas(oscar)
+imprimirNombreEnMayusculas({nombre: 'Juanito'})
+
+imprimirNombreyEdad(oscar)
+
+duplicate(oscar, dario)
+
+
+
