febaa478f2dc2f61763dd87e410c3498ea4d9ce7
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-09 21:49:26 -0600

    v6: hasta aquí se vio herencia y clases con javascript, me encanta esto

--- clase.js
+++ clase.js
@@ -1,37 +1,36 @@
-function Persona(nombre, apellido, altura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura= altura
+
+class Persona {
+    constructor(nombre, apellido, altura) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.altura = altura
+    }
+
+    saludar(){
+        console.log(`Hola, me llammo ${this.nombre} ${this.apellido}`)
+    }
+    soyalto(){
+        return this.altura > 1
+    }
+
 }
 
-function Transporte(tipo, color, precio){
-    this.tipo = tipo
-    this.color = color
-    this.precio = precio
-}
-
-Persona.prototype.saludar = function () {
-    console.log(`Hola, me llammo ${this.nombre} ${this.apellido}`)
-}
-
-Persona.prototype.soyalto = function() {
-    if (this.altura > 1){
-        console.log(`${this.nombre} es alt@`)
+class Programador extends Persona {
+    constructor(nombre, apellido, altura){
+        super(nombre, apellido, altura)
+    }
+    saludar(){
+        console.log(`Hola, mi nombre es ${this.nombre} y soy desarrollador`)
     }
 }
 
-Transporte.prototype.soyCaro = function() {
-    return this.precio > 100000
 
-} 
+
 
 
 
 var oscar = new Persona('Oscar', 'Eduardo', 1.72)
 var yolanda = new Persona('Yolanda', 'deLaRosa',0.4)
-var carro = new Transporte('carro','rojo', 37000 )
-var camion = new Transporte('camion', 'negro', 355000)
+var oscar2 = new Programador('Eduardo', 'de La Rosa', 1.75)
 
 
-oscar.soyalto()
-yolanda.soyalto()
